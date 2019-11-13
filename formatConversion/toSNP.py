3f4637dfe49f66125a8bc9569091e85f8f128153
Yilei Huang <yh362@cornell.edu>
2019-11-12 21:41:12 -0500

    clean up

--- formatConversion/toSNP.py
+++ formatConversion/toSNP.py
@@ -9,8 +9,8 @@
 # each row represents a SNP and each column a haplotype
 
 import argparse
-import numpy as np
 import copy
+import sys
 
 parser = argparse.ArgumentParser(description='prepare inputs to admi-simu program for two reference populations')
 parser.add_argument('-p1', action="store", dest="p1", type=str, help="path to SNP file for pop1")
@@ -29,11 +29,15 @@ with open(args.p1) as pop1, open(args.p2) as pop2, open(args.m) as map, \
     while pop1_line and pop2_line and map_line:
         chrom, snpid, gen_dist, phy_loc = map_line.strip().split('\t')
         pop1_info, pop2_info = pop1_line.strip().split(' '), pop2_line.strip().split(' ')
-        assert pop1_info[0] == pop2_info[0] and pop1_info[1] == pop2_info[1]
+
+        if pop1_info[0] != pop2_info[0] or pop1_info[1] != pop2_info[1]:
+            print(f'pop1 has SNP {pop1_info[0]} at physical position {pop1_info[1]}')
+            print(f'pop1 has SNP {pop2_info[0]} at physical position {pop2_info[1]}')
+            print(f'two reference population must have the same set of SNP data')
+            sys.exit()
+
 
         pop1_snp, pop2_snp = copy.deepcopy(pop1_info[2:]), copy.deepcopy(pop2_info[2:])
-        #print(pop1_snp)
-        #print(pop2_snp)
         allele_set = list(set(pop1_snp + pop2_snp))
 
         # only biallelic position is retained
@@ -42,16 +46,9 @@ with open(args.p1) as pop1, open(args.p2) as pop2, open(args.m) as map, \
             continue
 
         allele1, allele2 = allele_set[0], allele_set[1]
-        #temp = map(lambda x: str(1) if x == allele1 else str(0), pop1_snp)
-        #templist = list(map)
-        #print(f'templist is {templist}')
         snpout.write(f'{snpid}\t{chrom}\t{gen_dist}\t{phy_loc}\t{allele1}\t{allele2}\n')
-        #pop1out.write(''.join(map(lambda x: str(1) if x == allele1 else str(0), pop1_snp)))
-        #pop1out.write(''.join(map(str, np.array(pop1_snp) == allele1)))
         pop1out.write(''.join(['1' if allele == allele1 else '0' for allele in pop1_snp]))
         pop1out.write('\n')
-        #pop2out.write(''.join(map(lambda x: str(1) if x == allele1 else str(0), pop2_snp)))
-        #pop2out.write(''.join(map(str, np.array(pop2_snp) == allele1)))
         pop2out.write(''.join(['1' if allele == allele1 else '0' for allele in pop2_snp]))
         pop2out.write('\n')
 
