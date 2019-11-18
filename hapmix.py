1e07c14477605b5637278820ed3bc15f297b43de
Yilei Huang <yh362@cornell.edu>
2019-11-17 19:17:13 -0500

    preprocessing

---
+++ hapmix.py
@@ -0,0 +1,31 @@
+import argparse
+import sys
+import numpy
+
+def readEigenstrat(file):
+    '''
+    Read eigenstrat file. 
+    Input:
+        file: path to the eigenstrat file for a population. Each row represents a SNP, and each column represents a haplotype.
+    Output:
+        a numpy array version of the eigenstrat file.
+        Similarly, each row is a SNP, and each column a haplotype.
+    '''
+    with open(file) as f:
+        snps = f.readlines().strip().split('\n')
+        snps = [list(snp) for snp in snps]
+        return np.array(snps)
+
+def main():
+    parser = argparse.ArgumentParser(description='Local Ancestry Inference as implemented in Hapmix.')
+    parser.add_argument('-p1', action="store", dest="p1", type=str, help='eigenstrat SNP file for ancestral population 1 (eg. CEU)')
+    parser.add_argument('-p2', action="store", dest="p2", type=str, help='eigenstrat SNP file for ancestral population 2 (eg. YRI)')
+    parser.add_argument('-a', action="store", dest="a", type=str, help='eigenstrat SNP file for the admixed population.')
+    parser.add_argument('-mu', action="store", dest='mu', type=float, default=0.2, help='percentage of genetic composition of population 1 in the admixed population')
+    parser.add_argument('-t', action="store", dest='t', type=int, default=6, help='Time (in numbers of generation) to the admixture event')
+    args = parser.parse_args()
+
+    pop1_snp, pop2_snp, a_snp = readEigenstrat(args.p1), readEigenstrat(args.p2), readEigenstrat(args.a)
+
+if __name__ == '__main__':
+    main()
