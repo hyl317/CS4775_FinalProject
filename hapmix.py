3b81c6f505d4ba74fec415935eca536fe4da6f6d
Yilei Huang <yh362@cornell.edu>
2019-11-19 08:05:31 -0500

    output directory

--- hapmix.py
+++ hapmix.py
@@ -76,20 +76,21 @@ def main():
 
     hmmModel = hmm.HMM(pop1_snp, pop2_snp, args.mu, args.t, numSNP, n1, n2, rho1, rho2, theta1, theta2, D)
 
-    for i in range(a_snp.shape[1]):
-        states = hmmModel.decode(a_snp[:, i])
-        # find ancestry switching point
-        prev  = [states[0]] + states[:numSNP-1]
-        diff = np.array(states) - np.array(prev)
-        switch_points = np.where(diff != 0)[0]
-        print(switch_points)
-        report = ''
-        for point in switch_points:
-            report += f'{prev[point]}:{point-1} '
+    with open('decode.txt','w') as output:
+        for i in range(a_snp.shape[1]):
+            states = hmmModel.decode(a_snp[:, i])
+            # find ancestry switching point
+            prev  = [states[0]] + states[:numSNP-1]
+            diff = np.array(states) - np.array(prev)
+            switch_points = np.where(diff != 0)[0]
+            print(switch_points)
+            report = ''
+            for point in switch_points:
+                report += f'{prev[point]}:{point-1} '
         
-        if np.all(diff == 0) or switch_points[-1] != numSNP-1:
-            report += f'{states[-1]}:{numSNP-1}'
-        print(report)
+            if np.all(diff == 0) or switch_points[-1] != numSNP-1:
+                report += f'{states[-1]}:{numSNP-1}'
+            output.write(f'{report}\n')
 
 if __name__ == '__main__':
     main()
